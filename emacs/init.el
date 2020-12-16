c65516cf17abff58ab1671cf89832120ba01b70c
Benjamin Titmus <ben.titmus@cantab.net>
2020-12-16 13:14:16 +0000

    Emacs theme and cleanup

--- emacs/init.el
+++ emacs/init.el
@@ -26,10 +26,16 @@
           (package-install package)))
 
 ;; gui
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(column-number-mode t)
+(use-package emacs
+  :init
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1)
+  (column-number-mode t)
+  :config
+  (setq inhibit-splash-screen t))
+
 (set-frame-font "Triplicate T4c 12" nil t)
 
 ;; org-mode
@@ -41,6 +47,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (selenized-light)))
+ '(custom-safe-themes
+   (quote
+    ("cf36206431f80f0435bb7461e8be8429d949ba641abaf790b7730423e2e88638" "9bb86bf42ce13b9fce1690024d52238133988555009ac59a2f63ae2df7790c55" "59263e76fcbf0b0f278c27a78e01ad40d1191edf9c55cfcbe1a47e89e25a1893" default)))
  '(package-selected-packages (quote (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
