9fe42e4eecd9a0fbbe8280cc5c9c9ec33bb4ca22
Benjamin Titmus <ben.titmus@cantab.net>
2021-10-16 22:48:14 +0100

    Fix fish_prompt
    Latest version of fish does away with ^ for redirect to stderr
    so the old syntax in this function doesn't work anymore

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt --description 'Write out the prompt'
-  set __git_cb (set_color brown)(git branch ^/dev/null | grep \* | sed 's/* //')(set_color normal)"|"
+  set __git_cb (set_color brown)(git branch 2>/dev/null | grep \* | sed 's/* //')(set_color normal)"|"
   set -l dir (fish_prompt_dir $PWD)
   set -l __host_cb (fish_prompt_host)
   if [ $__host_cb != '' ]
