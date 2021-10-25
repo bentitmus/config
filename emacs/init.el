5d28f5055d252c2ce88c7827aae89e93da3047a4
Benjamin Titmus <ben.titmus@cantab.net>
2021-10-25 12:02:18 +0100

    Variable to set emacs location (work/home)

--- emacs/init.el
+++ emacs/init.el
@@ -36,13 +36,22 @@
   :config
   (setq inhibit-splash-screen t))
 
-;;(set-frame-font "Triplicate T4c 12" nil t)
-(set-face-attribute 'default nil :font "Triplicate T4c-14")
-(set-face-attribute 'fixed-pitch nil :font "Triplicate T4c-14")
-(set-face-attribute 'variable-pitch nil :font "Valkyrie T4-14")
+;; Find a way of detecting work set-up
+(setq bt-work-p nil)
+
+(if bt-work-p
+  (progn
+    (set-face-attribute 'default nil :font "Triplicate T4c-12")
+    (set-face-attribute 'fixed-pitch nil :font "Triplicate T4c-12")
+    (set-face-attribute 'variable-pitch nil :font "Valkyrie T4-12")
+    (setq bt-org-base "~/OrgMode"))
+  (progn
+    (set-face-attribute 'default nil :font "Triplicate T4c-14")
+    (set-face-attribute 'fixed-pitch nil :font "Triplicate T4c-14")
+    (set-face-attribute 'variable-pitch nil :font "Valkyrie T4-14")
+    (setq bt-org-base "~/Dropbox/OrgMode")))
 
 ;; org-mode
-(setq bt-org-base "~/Dropbox/OrgMode")
 (use-package org
   :ensure t)
 (use-package org-gtd
@@ -129,10 +138,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(selenized-light))
+ '(custom-enabled-themes (quote (selenized-light)))
  '(custom-safe-themes
-   '("cf36206431f80f0435bb7461e8be8429d949ba641abaf790b7730423e2e88638" "9bb86bf42ce13b9fce1690024d52238133988555009ac59a2f63ae2df7790c55" "59263e76fcbf0b0f278c27a78e01ad40d1191edf9c55cfcbe1a47e89e25a1893" default))
- '(package-selected-packages '(org-roam org-super-agenda org-gtd use-package)))
+   (quote
+    ("cf36206431f80f0435bb7461e8be8429d949ba641abaf790b7730423e2e88638" "9bb86bf42ce13b9fce1690024d52238133988555009ac59a2f63ae2df7790c55" "59263e76fcbf0b0f278c27a78e01ad40d1191edf9c55cfcbe1a47e89e25a1893" default)))
+ '(package-selected-packages (quote (org-roam org-super-agenda org-gtd use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
