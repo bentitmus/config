b7ecd3728156ec3186ef01b9c0775f4e9cb352da
Benjamin Titmus <ben.titmus@cantab.net>
2021-10-26 16:33:50 +0100

    Temporarily disable org-roam db sync until brought up

--- emacs/init.el
+++ emacs/init.el
@@ -106,9 +106,9 @@
   :after org
   :init
   (setq org-roam-directory (expand-file-name "OrgRoam" bt-org-base))
-  (setq org-roam-v2-ack t)
-  :config
-  (org-roam-db-autosync-mode))
+  (setq org-roam-v2-ack t))
+;;  :config
+;;  (org-roam-db-autosync-mode))
 
 ;; mu4e
 (if (file-directory-p "/opt/homebrew")
@@ -137,11 +137,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (selenized-light)))
+ '(custom-enabled-themes '(selenized-light))
  '(custom-safe-themes
-   (quote
-    ("cf36206431f80f0435bb7461e8be8429d949ba641abaf790b7730423e2e88638" "9bb86bf42ce13b9fce1690024d52238133988555009ac59a2f63ae2df7790c55" "59263e76fcbf0b0f278c27a78e01ad40d1191edf9c55cfcbe1a47e89e25a1893" default)))
- '(package-selected-packages (quote (org-roam org-super-agenda org-gtd use-package))))
+   '("cf36206431f80f0435bb7461e8be8429d949ba641abaf790b7730423e2e88638" "9bb86bf42ce13b9fce1690024d52238133988555009ac59a2f63ae2df7790c55" "59263e76fcbf0b0f278c27a78e01ad40d1191edf9c55cfcbe1a47e89e25a1893" default))
+ '(package-selected-packages '(org-roam org-super-agenda org-gtd use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
