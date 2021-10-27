3b03233efab299a469e8bfa245693616c4b1e189
Benjamin Titmus <ben.titmus@cantab.net>
2021-10-27 22:35:49 +0100

    Ignore errors from gpg tools

--- fish/functions/gpg-refresh.fish
+++ fish/functions/gpg-refresh.fish
@@ -1,4 +1,4 @@
 function gpg-refresh -a cmd -d "update GPG terminal"
-  command gpg-connect-agent updatestartuptty /bye
+  command gpg-connect-agent updatestartuptty /bye 2>/dev/null
   set -xg GPG_TTY (tty)
 end
