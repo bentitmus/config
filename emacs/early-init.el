f739e48a968e926428d12c11fea7cf9d6c82f072
Benjamin Titmus <ben.titmus@cantab.net>
2026-07-29 13:51:01 +0100

    fix: remove titlebar on `emacs`
    Signed-off-by: Benjamin Titmus <ben.titmus@cantab.net>

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,6 +1,11 @@
 ;; early-init.el is loaded earlier than init.el and used to setup the cache
 
+; use XDG cache path for the emacs cache
 (require 'xdg)
 (startup-redirect-eln-cache (expand-file-name "emacs/eln-cache" (xdg-cache-home)))
 (setq package-user-dir (expand-file-name "emacs/elpa" (xdg-cache-home)))
+
+; no title bar
+(add-to-list 'default-frame-alist '(undecorated . t))
+(setq frame-resize-pixelwise t)
 
